0677d4a3d28f271b12ab9503a9bc16e68a67b565
Edwin Mejia <eddwin@live.com>
2015-08-28 10:49:27 -0600

    changes

--- lib/iva_gem.rb
+++ lib/iva_gem.rb
@@ -1,5 +1,5 @@
 require "iva_gem/version"
-
+require 'iva_gem/iva_gem.rb'
 module IvaGem
   # Your code goes here...
 end
--- lib/iva_gem/version.rb
+++ lib/iva_gem/version.rb
@@ -1,3 +1,3 @@
 module IvaGem
-  VERSION = "0.1.0"
+  VERSION = "0.1.1"
 end
